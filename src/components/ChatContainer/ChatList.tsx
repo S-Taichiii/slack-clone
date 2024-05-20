74f885f2de3997b6901b91e70fd808964e7d1ead
S-Taichiii <ab20082@shibaura-it.ac.jp>
2024-05-20 16:46:39 +0900

    create add channel function

--- src/components/ChatContainer/ChatList.tsx
+++ src/components/ChatContainer/ChatList.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ChannelRef } from '../../type/Channel'
+import { subscribeChannels } from '../../features/channel/channelAPI'
+import ChannelCell from './ChannelCell';
+import ChannelAddModal from './ChannelAddModal';
 
 function ChatList() {
+    const [channelRefs, setChannelRefs] = useState<ChannelRef[]>([])
+    const [showModal, setShowModal] = useState<boolean>(false);
+    
+    useEffect(() => {
+        const unsubscribe = subscribeChannels(((channelRefs: ChannelRef[]) => {
+            setChannelRefs(channelRefs);
+        }));
+        
+        return () => unsubscribe();
+    }, []);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
   return (
     <div className='w-64 bg-gray-800'>
         <div className="py-4 px-2 border-b border-gray-300">
             <span className='font-bold text-gray-300'>チャンネル</span>
         </div>
         <div className='overflow-y-auto'>
-            <div className='py-1 px-4 hover:bg-gray-700'>
-                <p className='text-gray-300 hover:text-white'>#random</p>
-            </div>
+            {
+                channelRefs.map(({ channel, id }) => (
+                    <ChannelCell channel={channel} id={id} key={id} /> 
+                ))
+            }
         </div>
         <div className='px-4 py-2'>
-            <button className='text-gray-300 hover:text-white'>＋チャンネルを追加</button>
+            <button className='text-gray-300 hover:text-white' onClick={handleOpenModal}>＋チャンネルを追加</button>
+            {
+                showModal && <ChannelAddModal handleCloseModal={handleCloseModal} />
+            }
         </div>
     </div>
   )
