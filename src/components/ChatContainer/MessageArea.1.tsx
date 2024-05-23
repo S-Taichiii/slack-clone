e1fa35f666d374cbc3bf69beb34621c0970b7b9f
S-Taichiii <ab20082@shibaura-it.ac.jp>
2024-05-23 10:27:23 +0900

    create post message

--- src/components/ChatContainer/MessageArea.1.tsx
+++ src/components/ChatContainer/MessageArea.1.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState , KeyboardEvent, useEffect} from 'react'
+import React, { ChangeEvent, useState, KeyboardEvent, useEffect } from 'react';
 import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 import SendIcon from '@mui/icons-material/Send';
 import { MessageRef } from '../../type/Message';
@@ -6,41 +6,43 @@ import { useAppSelector } from '../../app/hooks';
 import { createMessage, subscribeMessage, postMessage } from '../../features/message/messageAPI';
 import MessageTile from './MessageTile';
 
-function MessageArea() {
+export function MessageArea() {
   const [messageRefs, setMessageRefs] = useState<MessageRef[]>([]);
   const channelId: string = useAppSelector((state) => state.channel.currentChannelId);
-  const userId = useAppSelector((state) => state.user.userId); 
-  
+  const userId = useAppSelector((state) => state.user.userId);
+
   const [message, setMessage] = useState('');
   const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
-  }
+  };
 
-  const sendMessage = async ()  => {
+  const sendMessage = async () => {
     if (userId) {
       try {
         // console.log(`userId: ${userId}, channelId: ${channelId}, message: ${message}`);
-        await postMessage(createMessage(userId ,channelId, message));
+        await postMessage(createMessage(userId, channelId, message));
         setMessage("");
       } catch (error) {
         console.error("Error sending message: ", error);
       }
     }
-  }
+  };
 
   const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
     if ((e.metaKey || e.ctrlKey) && e.code === "Enter") {
       sendMessage();
     }
-  }
+  };
 
   useEffect(() => {
     const unsubscribe = subscribeMessage(channelId, (messageRefs) => {
       setMessageRefs(messageRefs);
-    })
+    });
 
     return () => {
       unsubscribe();
+
+      console.log(messageRefs);
     };
   }, [channelId]);
 
@@ -48,28 +50,23 @@ function MessageArea() {
   return (
     <div className='flex-1 flex flex-col bg-gray-500 text-white'>
       <div className='p-4 m-3 overflow-y-auto'>
+        {messageRefs.map((messageRef) => (
+          <MessageTile message={messageRef.message} key={messageRef.id} />
+        ))}
       </div>
-        {
-          messageRefs.map((messageRef) => (
-            <MessageTile message={messageRef.message} key={messageRef.id} />
-          ))
-        }
-      <div className='px-4 py-2 bottom-0 bg-gray-900'>
+      <div className='mt-auto px-4 py-2 bottom-0 bg-gray-900'>
         <div className='flex items-center'>
           <TextareaAutosize
-            placeholder='# random へメッセージを入力'
+            placeholder='メッセージを入力'
             className='flex-1 bg-gray-700 text-white p-2 mx-2 rounded-lg focus:outline-none'
             onChange={handleInputChange}
             onKeyDown={handleKeyDown}
-            value={message}
-          />
+            value={message} />
           <button className='text-gray-400 hover:text-white' onClick={sendMessage}>
             <SendIcon />
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-export default MessageArea
